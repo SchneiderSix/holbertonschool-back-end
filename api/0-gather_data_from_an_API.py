405706e7b497a51be448cc9c83297bf3ed3eaa28
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-30 17:41:36 -0300

    Task 0 API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,35 +10,36 @@ import json
 from sys import argv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-        res = requests.get("https://jsonplaceholder.typicode.com/todos/?userId={}".format(argv[1]))
-        tsks = json.loads(res.text)
-        usrs = requests.get("https://jsonplaceholder.typicode.com/users")
-        us = json.loads(usrs.text)
+    res = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/?userId={}".format(argv[1])
+    )
+    tsks = json.loads(res.text)
+    usrs = requests.get("https://jsonplaceholder.typicode.com/users")
+    us = json.loads(usrs.text)
+    res_us = {}
+    counter = 0
+    counter_tsks = 0
+    nid = int(argv[1])
 
-        res_us  = {}
-        counter = 0
-        counter_tsks = 0
-        nid = int(argv[1])
+    for line in us:
+        counter += 1
+        res_us.update(line)
+        if counter == nid:
+            break
+    name_us = res_us["name"]
 
-        for line in us:
-                counter += 1
-                res_us.update(line)
-                if counter  == nid:
-                        break
+    my_tsks = [
+        (i["completed"], i["title"]) for i in tsks if "completed" or "title" in i
+    ]
 
-        name_us = res_us['name']
+    for i, j in my_tsks:
+        if i == True:
+            counter_tsks += 1
 
-        my_tsks = [(i['completed'], i['title']) for i in tsks if "completed" or "title" in i]
+    print("Employee {} is done with tasks({}/20):".format(name_us, counter_tsks))
 
-        for i, j in my_tsks:
-                if i == True:
-                        print(j)
-                        counter_tsks += 1
-
-        print("Employee {} is done with tasks({}/20):".format(name_us, counter_tsks))
-
-        for i, j in my_tsks:
-                if i == True:
-                        print("\t {}".format(j))
+    for i, j in my_tsks:
+        if i == True:
+            print("\t {}".format(j))
