df347b8dcf82117c80133e02cea1e26cbc90987b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-30 17:44:52 -0300

    Task 0 API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
-for a given employee ID,
-returns information about his/
-her TODO list progress.
+Module 0-gather_data_from_an_API
 """
 import requests
 import json
