05474ec148ef0bc50a60ad24f6c409453e64018a
SchneiderSix <https://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@github.com>
2022-12-01 10:17:41 -0300

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -29,7 +29,8 @@ if __name__ == "__main__":
     name_us = res_us["username"]
 
     my_tsks = [
-        (i["completed"], i["title"]) for i in tsks if "completed" or "title" in i
+        (i["completed"], i["title"]) for i in tsks
+        if "completed" or "title" in i
     ]
 
     for i, j in my_tsks:
@@ -44,10 +45,10 @@ if __name__ == "__main__":
             csv_list.append("False")
             csv_list.append("{}".format(j))
 
+
     print(name_us)
     print(csv_list)
 
-
-with open("{}".format(nid), "w") as fp:
-    writer = csv.writer(fp)
-    writer.writerow(csv_list)
+    with open('{}'.format(nid), 'w') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(csv_list)
